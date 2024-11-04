8c003e5f02d44303ed99ef532c103ffe979301f5
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-04 16:42:19 +0000

    unfinished single article page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import RootPage from "./pages/RootPage";
+import SingleArticlePage from "./pages/SingleArticlePage";
 
 export default function App() {
   return (
     <div className="main-layout">
       <Navbar />
-      <div className="page-wrapper">
+      <div className="page-wrapper content">
         <Routes>
           <Route path="/" element={<RootPage />} />
+          <Route path="/article/:article_id" element={<SingleArticlePage />} />
         </Routes>
       </div>
     </div>
---
+++ src/pages/SingleArticlePage.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticleById } from "../utils/api";
+import ArticleMeta from "../components/ArticleMeta";
+import CommentList from "../components/CommentList";
+
+const SingleArticlePage = () => {
+  const { article_id } = useParams();
+  const [article, setArticle] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getArticleById(article_id)
+      .then((data) => setArticle(data.article))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (isLoading) return <div>Todo spinner</div>;
+
+  const { article_img_url, title, body } = article;
+
+  return (
+    <>
+      <article className="content">
+        <div className="image-wrapper">
+          <img src={article_img_url} />
+        </div>
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </article>
+      <hr />
+      <ArticleMeta article={article} interactive />
+      <hr />
+      <CommentList />
+    </>
+  );
+};
+
+export default SingleArticlePage;
