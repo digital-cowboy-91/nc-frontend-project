1fb6c61f9e79b21f4024248293dd6ec50e87a0fe
digital-cowboy-91 <dave.kolaja@gmail.com>
2024-11-06 15:06:54 +0000

    handle queries

--- src/pages/SingleTopicPage.jsx
+++ src/pages/SingleTopicPage.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ArticleList from "../components/ArticleList";
 import { useParams } from "react-router-dom";
+import SortBar from "../components/SortBar";
 
 const SingleTopicPage = () => {
   const { slug } = useParams();
+  const [queries, setQueries] = useState({});
 
   return (
     <>
-      <ArticleList queries={{ topic: slug }} />
+      <SortBar onUpdateQuery={setQueries} />
+      <ArticleList queries={{ ...queries, topic: slug }} />
     </>
   );
 };
